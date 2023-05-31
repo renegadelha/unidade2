4028939f58013f877682d7bd0b57330f0659052f
renegadelha <renegadelha@gmail.com>
2023-05-31 20:07:38 -0300

    adicionei uns prints legais

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ produtos = ['celular xiaomi lite 9', 'celta lt', 'ar condicionado lg']
 resposta = gpt.consultarchatgpt(produtos[0])
 
 print(resposta)
+
+x = 2 + 3
+
+print(x)
+
+print('agora ta file')
